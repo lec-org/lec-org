dd70c537b663f485749b4bfaad34ac3e33e9c33f
trudbot <SYP2185759039@163.com>
2024-07-16 17:18:25 +0800

    fix: 未引用的声明-ts错误

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -5,8 +5,6 @@
 </template>
 
 <script setup lang="ts">
-	import HomeFirst from './views/index.vue'
-	import Introduction from './views/introduction.vue'
 	import Honor from '@/components/honor/honor.vue'
 </script>
 
