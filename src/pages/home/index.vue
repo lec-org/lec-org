a6ccce9b4c0e593bf45ee5eb31340066155f90f3
DX13279691 <DX13279691@meituan.com>
2024-07-17 15:47:57 +0800

    页面大概完成

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,13 +1,15 @@
 <template>
 	<div>
-		<HomeFirst />
+		<HomeFirst/>
 		<Introduction />
+		<StationDisplay />
 	</div>
 </template>
 
 <script setup lang="ts">
 	import HomeFirst from './views/index.vue'
 	import Introduction from './views/introduction.vue'
+	import StationDisplay from './views/StationDisplay.vue'
 </script>
 
 <style scoped></style>
